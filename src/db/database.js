192ca8a4d189b4daac414b869fc10af0d7287ad9
sunnyskyess420 <alively14@gmail.com>
2026-05-30 20:16:00 -0600

    Add files via upload

--- src/db/database.js
+++ src/db/database.js
@@ -3,8 +3,23 @@ import Dexie from 'dexie'
 // Create Dexie database
 const db = new Dexie('emotionMapperDB')
 
-// Define database schema
-// This creates a table called 'entries' with auto-incrementing ID
+// Define database schema with upgrade handler
+db.version(1).stores({
+  entries: '++id, emotion, intensity, note, createdAt'
+})
+
+db.version(2).stores({
+  entries: '++id, emotion, intensity, note, createdAt, physicalSensations, triggers, location, timeOfDay, copingStrategies, duration, socialContext, sleepQuality, energyLevel'
+})
+
+db.version(3).stores({
+  entries: '++id, emotions, intensity, note, createdAt, physicalSensations, triggers, location, timeOfDay, copingStrategies, duration, socialContext, sleepQuality, energyLevel'
+})
+
+db.version(4).stores({
+  entries: '++id, emotion, intensity, note, createdAt, physicalSensations, triggers, location, timeOfDay, copingStrategies, duration, socialContext, sleepQuality, energyLevel'
+})
+
 // Note: emotions, physicalSensations, and copingStrategies are not indexed because IndexedDB cannot index arrays
 db.version(5).stores({
   entries: '++id, emotion, intensity, note, createdAt, triggers, location, timeOfDay, duration, socialContext, sleepQuality, energyLevel'
