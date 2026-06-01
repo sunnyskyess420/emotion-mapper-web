4ee831e045ef3a3266b2c70fa5009d85e95f1937
sunnyskyess420 <sunnyskyess420@users.noreply.github.com>
2026-05-31 18:21:57 -0600

    Add Dexie Cloud auth integration

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   base: './',
+  build: {
+    rolldownOptions: {
+      external: ['dexie-cloud-addon']
+    }
+  }
 })
